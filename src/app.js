fd56c942a1599a96b4c86591389d9abe137f84b1
isolinadurand <isolinadurand@gmail.com>
2023-02-14 15:47:36 -0300

    agrego css

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app= express();
+const path = require('path');
+
 const port = 3000;
+
 const mainRoutes = require('./routers/mainRoutes');
 const usersRoutes = require('./routers/usersRoutes');
+const publicPaht= path.join(__dirname,'../public')
 
-app.use(express.static('./public'));
+app.use(express.static(publicPaht));
 
 app.listen(port, ()=> {
     console.log(`Servidor corriendo en el puerto ${port}`);
