8067e8eb928bba63f89dd8abe8d95ca9fedcf5ec
isolinadurand <isolinadurand@gmail.com>
2023-02-18 16:36:56 -0300

    agregando crud

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,21 +1,27 @@
+const fs = require('fs');
 const path = require('path');
+
+
+
 
 const login = path.join(__dirname,'../views/users/login.ejs');
 const register = path.join(__dirname, '../views/users/register.ejs');
 const userList = path.join(__dirname,'../views/users/usersList.ejs');
 const usersRes = path.join(__dirname, '../views/users/usersResults.ejs');
 const usersEdit = path.join(__dirname, '../views/users/usersEdit.ejs');
-const users= [
-    {id: 1 , name:'Dario'},
-    {id: 2 , name:'Javier'},
-    {id: 3 , name: 'Iso'},
-    {id: 4 , name: 'Clari'},
-    {id: 5 , name:'Matu'},
-    {id: 6 , name: 'Jorge'},
-    {id: 7 , name:'Parde'},
-];
+
 
 const usersController = {
+    readFile: function(){
+        let usersFile = fs.readFileSync('users.json',{encoding:'utf-8'});
+        let users;
+        if (usersFile == ""){
+             return users = [];
+        } else {
+             return users = JSON.parse(usersFile);
+         }
+    },
+
     login: (req, res) => {
         res.render(login);
     },
@@ -23,20 +29,35 @@ const usersController = {
         res.render(register);
     },
     save: (req, res) => {
-        let nuevoUsuario = req.body;
-        // guarda la info en el modelo
+        let newUser = {
+            name: req.body.name,
+            edad: req.body.edad,
+            email: req.body.email
+        }
+        let users = usersController.readFile();
+      
+      users.push(newUser);
+      let usersJSON = JSON.stringify(users);
+
+      fs.writeFileSync('users.json', usersJSON);
+
+
+
     
         //redirije a listado de usuarios
         res.redirect('/users/list');
 
     },
     list: (req, res) => {
-       
+        let users = usersController.readFile();
+              
         res.render(userList, {'users': users});
     },
     search: (req, res) => {
         let loQueBuscoElUsuario = req.query.searchBar;
         let usersResults = [];
+        let users = usersController.readFile();
+       
         for (let i=0 ; i< users.length; i++) {
             if (users[i].name.includes(loQueBuscoElUsuario)) {
                 usersResults.push(users[i]);
@@ -48,22 +69,31 @@ const usersController = {
         
     },
     edit: (req, res) => {
+        let users = usersController.readFile();
+        
         let userId = req.params.userId;
+ 
         let userToEdit= users[parseInt(userId)-1];
+
        res.render(usersEdit, {userToEdit : userToEdit})
 
 
 
     },
     update: (req, res) => {
-        res.send("ingresé por put");
+        res.send('entre por update a modificar')
+        //redirije a listado de usuarios
+  //      res.redirect('/users/list');
+
+       
         //se supone que grabo la actualización en la base de datos
     },
     delete: (req, res) => {
          let id = parseInt(req.params.userId);
-         res.send(`Acá se borra el usuario número ${id} de nombre ${users[id - 1].name} `);
+         res.send(`Acá se borra el usuario número ${id} `);
         //acá se borrará el dato de la base de datos.
-    }
+    },
+  
 }
 
 module.exports = usersController;
